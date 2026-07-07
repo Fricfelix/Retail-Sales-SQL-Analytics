37e69c67c366dbdb7fcec37f3a41e9ba6611b5bf
Fricfelix <fricfelix@gmail.com>
2026-07-08 00:42:23 +0100

    Create project database

---
+++ Sql/01_create_database.sql
@@ -0,0 +1,11 @@
+-- ============================================
+-- Retail Sales SQL Analytics Project
+-- Create Database
+-- Author: Ifeanyi Ogbodo
+-- ============================================
+
+DROP DATABASE IF EXISTS retail_sales_db;
+
+CREATE DATABASE retail_sales_db;
+
+USE retail_sales_db;
